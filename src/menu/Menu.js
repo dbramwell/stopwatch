44e778e0def7bbb4cbc3ec1bbaee721cde4c92f6
bram <david.j.bramwell@googlemail.com>
2017-05-04 20:19:03 +1200

    Finished menu functionality

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 
 class Menu extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: "timer"
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(event) {
+    const newActive = this.state.active === "timer" ? "stopwatch" : "timer";
+    this.setState({active: newActive});
+    this.props.onClick(newActive);
+  }
+
   render() {
     return (
       <div className="menu">
-        <button>TIMER</button>
-        <button>STOPWATCH</button>
+        <button disabled={this.state.active === "timer"} onClick={this.onClick}>TIMER</button>
+        <button disabled={this.state.active === "stopwatch"} onClick={this.onClick}>STOPWATCH</button>
       </div>
     );
   }
--- src/menu/Menu.test.js
+++ src/menu/Menu.test.js
@@ -20,3 +20,58 @@ it('buttons are named "TIMER" and "STOPWATCH" on initial rendering', () => {
   expect(buttons.at(0).text()).toEqual("TIMER");
   expect(buttons.at(1).text()).toEqual("STOPWATCH");
 });
+
+it('props.onClick is called with "stopwatch" when STOPWATCH is clicked', () => {
+  const mockFunction = jest.fn();
+  const menu = shallow(<Menu onClick={mockFunction}/>);
+  const stopwatchButton = menu.find('button').at(1);
+  stopwatchButton.simulate("click");
+  expect(mockFunction.mock.calls).toEqual([["stopwatch"]]);
+});
+
+it('props.onClick is called with "timer" when TIMER is clicked', () => {
+  const mockFunction = jest.fn();
+  const menu = shallow(<Menu onClick={mockFunction}/>);
+  menu.setState({active: 'stopwatch'});
+  const timerButton = menu.find('button').at(0);
+  timerButton.simulate("click");
+  expect(mockFunction.mock.calls).toEqual([["timer"]]);
+});
+
+it('state.active is set to stopwatch when STOPWATCH is clicked', () => {
+  const mockFunction = jest.fn();
+  const menu = shallow(<Menu onClick={mockFunction}/>);
+  const stopwatchButton = menu.find('button').at(1);
+  stopwatchButton.simulate("click");
+  expect(mockFunction.mock.calls.length).toBe(1);
+  expect(menu.state().active).toEqual("stopwatch");
+});
+
+it('state.active is set to timer when TIMER is clicked', () => {
+  const mockFunction = jest.fn();
+  const menu = shallow(<Menu onClick={mockFunction}/>);
+  menu.setState({active: 'stopwatch'});
+  const timerButton = menu.find('button').at(0);
+  timerButton.simulate("click");
+  expect(mockFunction.mock.calls.length).toBe(1);
+  expect(menu.state().active).toEqual("timer");
+});
+
+it('when state.active is set to timer, TIMER button is disabled and STOPWATCH button is enabled', () => {
+  const mockFunction = jest.fn();
+  const menu = shallow(<Menu onClick={mockFunction}/>);
+  const timerButton = menu.find('button').at(0);
+  const stopwatch = menu.find('button').at(1);
+  expect(timerButton.html().includes('disabled=""')).toBeTruthy();
+  expect(stopwatch.html().includes('disabled=""')).toBeFalsy();
+});
+
+it('when state.active is set to stopwatch, STOPWATCH button is disabled', () => {
+  const mockFunction = jest.fn();
+  const menu = shallow(<Menu onClick={mockFunction}/>);
+  menu.setState({active: 'stopwatch'});
+  const timerButton = menu.find('button').at(0);
+  const stopwatch = menu.find('button').at(1);
+  expect(timerButton.html().includes('disabled=""')).toBeFalsy();
+  expect(stopwatch.html().includes('disabled=""')).toBeTruthy();
+});
